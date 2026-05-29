510bc3bd5aa3ed9d899389687a38bb6ab2a8d796
Bhaumik1234-star <bhaumikrathore@riccarton.school.nz>
2026-05-29 21:19:30 +1200

    Added car movement and steering
    Added basic car movement and steering system in Godot. The car can accelerate , slow down the friction, and turn using keyboard input. Steering becomes less senstitive at higher speeds to make the movement easier to control.

--- Scenes/Car/Car.gd
+++ Scenes/Car/Car.gd
@@ -1,5 +1,27 @@
 extends Area2D
 
+# Top Speed of the car
+@export var max_speed: float = 380.0
+
+# How fast car slows down
+@export var friction: float = 300.0
+
+# How fast the car speeds up
+@export var acceleration: float = 150.0
+
+# How fast the car turns
+@export var steer_strenght: float = 6.0
+
+# Minimum turing power at high speed
+@export var min_steer_factor: float = 0.5
+
+# Player input
+var _throttle: float = 0.0
+var _steer: float = 0.0
+
+# Start the car speed at 0
+var _velocity: float = 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +29,42 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	# Get input from players
+	_throttle = Input.get_action_strength("ui_up")
+	_steer = Input.get_axis("ui_left", "ui_right")
+	
+
+
+func _physics_process(delta: float) -> void:
+	# Move car faster or slower
+	apply_throttle(delta)
+	# Turn the car
+	apply_rotation(delta)
+	# Move forward
+	position += transform.x * _velocity * delta	
+	
+func apply_throttle(delta: float) -> void:
+	if _throttle > 0.0:
+		# Speed up
+		_velocity += acceleration * delta
+	else:
+		# Slow down
+		_velocity -= friction * delta
+	# Keep speed bettwen 0 and max_speed
+	_velocity = clampf(_velocity, 0.0, max_speed)
+	
+	
+func get_steer_factor() -> float:
+	# Make the steering senstivity lower when the car is moving faster
+	return clampf(
+		1.0 - pow(_velocity / max_speed, 2.0),
+		min_steer_factor,
+		1.0
+	)
+	
+	
+func apply_rotation(delta: float) -> void:
+	# Turn the car left or right based on player input
+	rotate(steer_strenght * delta * _steer)
+ 
